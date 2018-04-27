0e1528e8138b8c3c136934373c76e1adece56975
saifdev1642 <saifdev1642@gmail.com>
2018-04-27 06:13:05 +0600

    set up for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,11 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp',(err,db)=>{
-    if(err){
-        return console.log('Unable to connect',err);
-    }
-    console.log('Successfully Connected to MongoDB server');
-});
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp')
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 const {mongoose} = require('./db/mongoose');
 const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
+
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json());
+
+
 
 app.post('/todos',(req,res,next)=>{
    var todo = new Todo({text:req.body.text});
@@ -29,10 +33,32 @@ app.get('/todos',(req,res) => {
    })
 })
 
-const Port = 3000;
-app.listen(Port,()=>{
-   console.log('App started at Port: '+Port);
+//Get Individual Todo
+app.get('/todos/:id',(req,res)=>{
+    var id = req.params.id;
+    if(!ObjectID.isValid(id)){
+        res.status(404).send();
+    }
+    Todo.findById(id).then((todo)=>{
+         if(todo){
+             res.send({todo});
+         }else{
+             res.status(404).send();
+         }
+    },(e)=>{
+        res.status(400).send();
+    })
 })
+
+
+
+// app.listen(Port,()=>{
+//    console.log('App started at Port: '+Port);
+// })
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log('listening on', http.address().port);
+  });
 
 module.exports = {app};
 
